3aa5c47c9b4ecca7299f345eb9aacb654c0d679f
tobykentrunzhi <396367738@qq.com>
2015-10-30 19:56:40 +0800

    Initial Commit

---
+++ Crazy12/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crazy12
+//
+//  Created by Arron on 10/30/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crazy12/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crazy12
+//
+//  Created by Arron on 10/30/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
